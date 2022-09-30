be6248c72591ae56776f35d09526b1b90d9c7186
Serhat Oğuz <sserhatoguz@outlook.com>
2022-10-01 00:08:38 +0300

    Add project files.

---
+++ WindowsFormsApp3/hesapla.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp3
+{
+    internal class hesapla
+    {
+        public int topla(int a, int b)
+        {
+            return a + b;
+        }
+        public int cikar(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
